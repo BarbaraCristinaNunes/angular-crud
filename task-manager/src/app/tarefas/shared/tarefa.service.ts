50761df5d98deaba7bf22c093774c6b5e621ff85
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2022-02-14 15:57:35 +0100

    created service's methods

--- task-manager/src/app/tarefas/shared/tarefa.service.ts
+++ task-manager/src/app/tarefas/shared/tarefa.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Tarefa } from '.';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,54 @@ import { Injectable } from '@angular/core';
 export class TarefaService {
 
   constructor() { }
+
+  listartTodos(): Tarefa[]
+  {
+    const tarefas = localStorage['tarefas'];
+    return tarefas ? JSON.parse(tarefas): [];
+  }
+
+  cadastrar(tarefa: Tarefa): void
+  {
+    const tarefas = this.listartTodos();
+    tarefa.id = new Date().getTime();
+    tarefas.push(tarefa);
+    localStorage['tarefas'] = JSON.stringify(tarefas);
+  }
+
+  buscarPorID(id: number): Tarefa
+  {
+    const tarefas: Tarefa [] = this.listartTodos();
+    return tarefas.find(tarefa => tarefa.id === id);
+  }
+
+  atualizar(tarefa: Tarefa): void
+  {
+    const tarefas: Tarefa [] = this.listartTodos();
+    tarefas.forEach((obj, index, objs) => {
+      if(tarefa.id === obj.id) {
+        objs[index] = tarefa;
+      }
+    });
+    localStorage['tarefas'] = JSON.stringify(tarefas);
+  }
+
+  remover(id: number): void
+  {
+    let tarefas: Tarefa [] = this.listartTodos();
+    tarefas = tarefas.filter(tarefa => tarefa.id !== id);
+    localStorage['tarefa'] = JSON.stringify(tarefas);
+
+  }
+
+  alterarStatus(id: number): void
+  {
+    const tarefas: Tarefa [] = this.listartTodos();
+    tarefas.forEach((obj, index, objs) => {
+      if(id === obj.id){
+        objs[index].concluida = !obj.concluida;
+      }
+    });
+    localStorage['tarefa'] = JSON.stringify(tarefas);
+  }
 }
