7829b9829bccedd21205c7a2187707005e0702b1
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2022-02-15 16:43:02 +0100

    Create tarefas-routing.module.ts

---
+++ task-manager/src/app/tarefas/tarefas-routing.module.ts
@@ -0,0 +1,9 @@
+import { Routes } from "@angular/router";
+import { ListarTarefaComponent } from "./listar";
+
+export const TarefasRoutes: Routes = [
+    {
+        path: 'tarefas',
+        redirectTo: 'tarefas/listar'
+    }
+]
