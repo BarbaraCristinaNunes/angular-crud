48517f7974e170587c498aadf6ccf6f39a44ebb2
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2022-02-16 14:07:22 +0100

    created method remover

--- task-manager/src/app/tarefas/listar/listar-tarefa.component.ts
+++ task-manager/src/app/tarefas/listar/listar-tarefa.component.ts
@@ -22,4 +22,12 @@ export class ListarTarefaComponent implements OnInit {
     return this.tarefaService.listartTodos();
   }
   
+  remover($event: any, tarefa: Tarefa): void
+  {
+    $event.preventDefault();
+    if(confirm('Deseja remover a tarefa ' + tarefa.nome + '?')){
+      this.tarefaService.remover(tarefa.id);
+      this.tarefas = this.listarTodos();
+    }
+  }
 }
